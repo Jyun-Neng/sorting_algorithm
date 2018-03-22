c3e250045daeb7d1914edaabce9e8ed4b65501df
Jyun-NengJi <jyunnengji@gmail.com>
2018-03-22 22:07:13 +0800

    init

---
+++ insertion_sort.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def insertion_sort(num_seq):
+    """
+    Insertion sort algorithm.
+    Input: number sequence
+    Output: number sequence (ascending order)
+    Complexity: O(n^2)
+    """
+    sorted_seq = []
+    for num in num_seq:
+        size = len(sorted_seq)
+        for j in range(size):
+            if num < sorted_seq[j]:
+                num, sorted_seq[j] = sorted_seq[j], num
+        sorted_seq.append(num)
+    return np.asarray(sorted_seq)
+
+
+if __name__ == '__main__':
+    low, high, size = 0, 100, 20
+    num_seq = np.random.randint(low, high, size)
+    sorted_seq = insertion_sort(num_seq)
+    print('----before sort----')
+    print(num_seq)
+    print('----after sort----')
+    print(sorted_seq)
